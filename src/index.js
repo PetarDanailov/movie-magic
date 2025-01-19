9ec847ce83fb9c6c3baf6a946ec75a7e12033cb6
PetarDanailov <danailovvpetar@gmail.com>
2025-01-19 19:15:37 +0200

    Fixed rating and made it dynamic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import handlebars from "express-handlebars";
 import router from "./routes.js";
+import showRatingHelper from "./helpers/ratingHelper.js";
 
 const app = express();
 app.engine("hbs", handlebars.engine({
-  extname: "hbs"
+  extname: "hbs",
+  helpers:{
+    showRating: showRatingHelper
+  }
 }))
 app.set("view engine", "hbs")
 app.set("views", "./src/views")
