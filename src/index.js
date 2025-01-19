a926af8b3e551548b4762fa4e031f35686774117
PetarDanailov <danailovvpetar@gmail.com>
2025-01-19 11:51:13 +0200

    Set handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import handlebars from "express-handlebars";
 
 const app = express();
+app.engine("hbs", handlebars.engine({
+  extname: "hbs"
+}))
+app.set("view engine", "hbs")
+app.set("views", "./src/views")
 
 app.get('/', (req,res) => {
-  res.send("It works");
+  res.render("home", {layout: false});
 })
 
 app.listen(5000, () => {
