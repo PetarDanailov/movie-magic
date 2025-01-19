220883140dce6a80cd078d2e2c483b0fcec3fb09
PetarDanailov <danailovvpetar@gmail.com>
2025-01-19 19:42:34 +0200

    Finished filtratiton

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -10,9 +10,15 @@ export function create(movieData){
     ...movieData})
 }
 export function getAll(filter = {}){
-  const result = movies
+  let result = movies
   if(filter.search){
     result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()))
+  }
+  if(filter.genre){
+    result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase())
+  }
+  if(filter.year){
+    result = result.filter(movie => movie.year === filter.year)
   }
   return result;
 }
